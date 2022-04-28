1016ace37a858d9ebeb1c73771b4867183516b1e
Max Kusatz <max@trialserver.de>
2022-04-28 10:56:36 +0200

    changed zoom to other link

--- zoom.py
+++ zoom.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import pyautogui as py
+#import pyautogui as py
 import time
  
-passcode = "5wSAfu"
-meet_code = "849 8490 0680"
+passcode = "309705"
+meet_code = "945 6875 1227"
 name = "office1"
  
 def join(meet, password):
@@ -20,43 +20,40 @@ def join(meet, password):
     })
     driver = webdriver.Chrome(options=opt)
     #driver = webdriver.Chrome('C://software/chromedriver.exe')
-    driver.get('https://zoom.us/join')
-          
-    time.sleep(5)
-    driver.find_element_by_xpath("//input[@id='join-confno']").send_keys(meet_code)
+    driver.get(' https://NTNU.zoom.us/j/94568751227?pwd=c3JmQWxmWjdWQXU2S2J0SDVOWnRYQT09')
     
     time.sleep(5) #to let the webpage open completely
-    driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/button[2]").click()
-    
-    time.sleep(5)
-    driver.find_element_by_xpath("//a[@id='btnSubmit']").click()
- 
-    time.sleep(5)
-    driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div[2]/h3[1]/span/a").click()
+    driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div[2]/h3[2]/span/a").click()
     
     time.sleep(5)
     driver.find_element_by_xpath("//*[@id='inputname']").send_keys(name)
+ 
+    time.sleep(5)
+    driver.find_element_by_xpath("//*[@id='joinBtn']").click()
     
     time.sleep(5)
-    driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div[1]/form/div/div[4]/div").click()
+    #driver.find_element_by_xpath("//*[@id='inputname']").send_keys(name)
     
-    time.sleep(2)
-    driver.find_element_by_xpath("//*[@id='wc_agree1']").click()
-    time.sleep(2)
+    #time.sleep(5)
+    #driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div[1]/form/div/div[4]/div").click()
     
-    print("before pass")
+    #time.sleep(2)
+    #driver.find_element_by_xpath("//*[@id='wc_agree1']").click()
+    #time.sleep(2)
+    
+    #print("before pass")
     # enter passcode
-    enter_passcode = py.locateCenterOnScreen('passc.png')
-    py.moveTo(enter_passcode)
-    py.click()
-    py.write(passcode)
-    print("after pass")
+    #enter_passcode = py.locateCenterOnScreen('passc.png')
+    #py.moveTo(enter_passcode)
+    #py.click()
+    #py.write(passcode)
+    #print("after pass")
 
  
     # join the meeting
-    time.sleep(5)
-    btn = py.locateCenterOnScreen("join.png")
-    py.moveTo(btn)
-    py.click()
+    #time.sleep(5)
+    #btn = py.locateCenterOnScreen("join.png")
+    #py.moveTo(btn)
+    #py.click()
  
 join(meet_code,passcode)
